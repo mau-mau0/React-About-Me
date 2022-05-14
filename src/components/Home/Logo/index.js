a87ec0842c718b295c286fde139fe2ae9a71b940
Kamau Bowie <k.maumau11@gmail.com>
2022-05-14 00:43:21 -0500

    minor adjustments

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,64 +1,24 @@
-import { useEffect, useRef } from 'react'
-import gsap from 'gsap-trial'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
+import { useRef } from 'react'
 import LogoS from '../../../assets/images/headshot1.png'
 import './index.scss'
 
 const Logo = () => {
-  const bgRef = useRef()
-  const outlineLogoRef = useRef()
   const solidLogoRef = useRef()
 
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin)
-
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 20,
-      })
-
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 3,
-        duration: 4,
-      }
-    )
-  }, [])
-
   return (
-    <div className="logo-container" ref={bgRef}>
-      <img
-        className="solid-logo"
-        ref={solidLogoRef}
-        src={LogoS}
-        alt="JavaScript,  Developer"
-      />
-      <img
-        className="solid-logo"
-        ref={solidLogoRef}
-        src={LogoS}
-        alt="JavaScript,  Developer"
-      />
-
-      <svg version="1.1" id="circle" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-        viewBox="0 0 100 250">
-
-        <circle fill="none" stroke="#f37867" stroke-width="3" stroke-mitterlimit="0" cx="50" cy="50" r="48" stroke-dasharray="360" stroke-linecap="round" transform="rotate(-90 ) translate(-100 0)" >
-          <animate attributeName="stroke-dashoffset" values="360;0" dur="5s" repeatCount="1"></animate>
-        </circle>
+    <div class='circleanime'>
+      <svg height="45em" width="40em" viewBox="0 0 1 563">
+        <circle class="circle" cx="50%" cy="50%" r="227" stroke="#f37867" stroke-width="13" fill-opacity="0" />
       </svg>
-
+      <div class='logo'></div>
+      <div class='logoimg'>
+        <img
+          className="solid-logo"
+          ref={solidLogoRef}
+          src={LogoS}
+          alt="JavaScript,  Developer"
+        />
+      </div>
     </div>
   )
 }
